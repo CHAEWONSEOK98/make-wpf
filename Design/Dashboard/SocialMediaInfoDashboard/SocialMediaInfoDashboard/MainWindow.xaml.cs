12a9c16c57fb7e8697a16343d663ef7bd09ef7cf
CHAEWONSEOK98 <7dugod7@naver.com>
2024-07-19 23:48:36 +0900

    SocialMediaInfoDashboard

---
+++ Design/Dashboard/SocialMediaInfoDashboard/SocialMediaInfoDashboard/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+
+namespace SocialMediaInfoDashboard
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+    }
+}
