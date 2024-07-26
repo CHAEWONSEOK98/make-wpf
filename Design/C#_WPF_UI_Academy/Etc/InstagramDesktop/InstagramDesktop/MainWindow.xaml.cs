61a86cffbe58ac4756bbf72ea064a01ec80770ed
CHAEWONSEOK98 <7dugod7@naver.com>
2024-07-26 20:24:15 +0900

    InstagramDesktop

---
+++ Design/C#_WPF_UI_Academy/Etc/InstagramDesktop/InstagramDesktop/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+
+namespace InstagramDesktop
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void textSearch_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            txtSearch.Focus();
+        }
+
+        private void txtSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if(!string.IsNullOrEmpty(txtSearch.Text) && txtSearch.Text.Length > 0)
+            {
+                textSearch.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                textSearch.Visibility= Visibility.Visible;
+            }
+        }
+    }
+}
