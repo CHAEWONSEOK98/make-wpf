29770c6a38d3b0485d5835c71958fad8933909e8
CHAEWONSEOK98 <7dugod7@naver.com>
2024-07-28 04:30:38 +0900

    ControlTemplateBasic

---
+++ Learn/WpfBasics/ControlTemplateBasic/ControlTemplateBasic/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+
+namespace ControlTemplateBasic
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainViewModel();
+        }
+    }
+}
---
+++ Learn/WpfBasics/ControlTemplateBasic/ControlTemplateBasic/ViewModels/MainViewModel.cs
@@ -0,0 +1,14 @@
+﻿using ControlTemplateBasic.Models;
+
+namespace ControlTemplateBasic
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public MainViewModel()
+        {
+            Items = ComboItem.Items;
+        }
+
+        public List<ComboItem> Items { get; set; }
+    }
+}
