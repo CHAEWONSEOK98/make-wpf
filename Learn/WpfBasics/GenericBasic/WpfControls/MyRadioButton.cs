093d71711b298069e85b6ca37b0ec658adcf0b66
CHAEWONSEOK98 <7dugod7@naver.com>
2024-07-29 09:18:14 +0900

    GenericBasic

---
+++ Learn/WpfBasics/GenericBasic/WpfControls/MyRadioButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfControls
+{
+    public class MyRadioButton : RadioButton
+    {
+        static MyRadioButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(MyRadioButton), new FrameworkPropertyMetadata(typeof(MyRadioButton)));
+        }
+    }
+}
