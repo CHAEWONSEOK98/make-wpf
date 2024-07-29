742e4a10b0cd49930ec8e1da6f2b93969839f382
CHAEWONSEOK98 <7dugod7@naver.com>
2024-07-29 23:41:14 +0900

    kakao - create TitleBar

---
+++ Learn/KakaoTalk/KakaoTalk/App.xaml.cs
@@ -0,0 +1,47 @@
+﻿using KakaoTalk.Views;
+using Microsoft.Extensions.DependencyInjection;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace KakaoTalk
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            Services = ConfigureServices();
+            Startup += App_Startup;
+        }
+
+        private void App_Startup(object sender, StartupEventArgs e)
+        {
+            var mainView = App.Current.Services.GetService<MainView>()!;
+            mainView.Show();
+        }
+
+        public new static App Current => (App)Application.Current;
+
+        public IServiceProvider Services { get; }
+
+        private static IServiceProvider ConfigureServices()
+        {
+            var services = new ServiceCollection();
+
+            // Service
+            // services.AddSingleton<ITestService, TestService>();
+
+            // Views
+            services.AddSingleton<MainView>();
+
+            //services.AddSingleton<IFilesService, FilesService>();
+            //services.AddSingleton<ISettingsService, SettingsService>();
+            //services.AddSingleton<IClipboardService, ClipboardService>();
+            //services.AddSingleton<IShareService, ShareService>();
+            //services.AddSingleton<IEmailService, EmailService>();
+
+            return services.BuildServiceProvider();
+        }
+    }
+
+}
---
+++ Learn/KakaoTalk/KakaoTalk/Controls/TitleBar.xaml.cs
@@ -0,0 +1,71 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+
+using WpfLib.Extensions;
+
+namespace KakaoTalk.Controls
+{
+    [ObservableObject]
+
+    public partial class TitleBar : UserControl
+    {
+        private Window? _parentWindow;
+        private WindowState _winState;
+
+        public WindowState Winstate
+        {
+            get 
+            { 
+                return _winState; 
+            }
+            set 
+            { 
+                SetProperty(ref _winState, value); 
+            }
+        }
+
+
+        public Window ParentWindow
+        {
+            get
+            {
+                if(_parentWindow == null)
+                {
+                    _parentWindow = this.FindParent<Window>()!;
+                }
+                return _parentWindow;
+            }
+            set { _parentWindow = value; }
+        }
+
+
+        public TitleBar()
+        {
+            InitializeComponent();
+
+            btnExit.Click += BtnExit_Click;
+            btnMaximize.Click += BtnMaximize_Click;
+            btnMinimize.Click += BtnMinimize_Click;
+        }
+
+        private void BtnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            Winstate = WindowState.Minimized;
+            ParentWindow.WindowState = Winstate;
+        }
+
+        private void BtnMaximize_Click(object sender, RoutedEventArgs e)
+        {
+            Winstate = ParentWindow.WindowState == WindowState.Maximized
+                ? WindowState.Normal
+                : WindowState.Maximized;
+            ParentWindow.WindowState = Winstate;
+        }
+
+        private void BtnExit_Click(object sender, RoutedEventArgs e)
+        {
+            ParentWindow.Close();
+        }
+    }
+}
