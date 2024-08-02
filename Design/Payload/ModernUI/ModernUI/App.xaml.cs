167acf76a9b55711d5c037914208a63ce0e9f6b7
CHAEWONSEOK98 <7dugod7@naver.com>
2024-08-03 06:53:09 +0900

    Design - ModernUI

---
+++ Design/Payload/ModernUI/ModernUI/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+
+namespace ModernUI
+{
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Design/Payload/ModernUI/ModernUI/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+
+namespace ModernUI
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+    }
+}
