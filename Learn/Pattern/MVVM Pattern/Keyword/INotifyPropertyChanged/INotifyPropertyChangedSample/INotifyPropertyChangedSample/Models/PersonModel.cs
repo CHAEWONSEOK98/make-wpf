45c012579a0d3bd4d58c623ab91ecbc8f0b1c10d
CHAEWONSEOK98 <7dugod7@naver.com>
2024-08-03 05:48:59 +0900

    MVVM Pattern Keyword - INotifyPropertyChanged, Databinding

---
+++ Learn/Pattern/MVVM Pattern/Keyword/INotifyPropertyChanged/INotifyPropertyChangedSample/INotifyPropertyChangedSample/Models/PersonModel.cs
@@ -0,0 +1,32 @@
+﻿
+
+namespace INotifyPropertyChangedSample.Models
+{
+    internal class PersonModel : ObservableObject
+    {
+        private string _name;
+
+		public string Name
+		{
+			get { return _name; }
+			set 
+            {
+                _name = value;
+                OnPropertyChanged();
+            }
+		}
+
+        public PersonModel()
+        {
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    Random rd = new Random();
+                    Name = rd.Next(1, 1000).ToString();
+                    Thread.Sleep(1000);
+                }
+            });
+        }
+    }
+}
