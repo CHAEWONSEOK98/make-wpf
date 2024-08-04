f2c00c885cb8b33c96ab45484964d7e5bde59a05
CHAEWONSEOK98 <7dugod7@naver.com>
2024-08-04 13:41:52 +0900

    WpfBasics - DataBindingBasic

---
+++ Learn/WpfBasics/DataBindingBasic/DataBindingBasic/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+
+
+namespace DataBindingBasic
+{
+    public partial class MainWindow : Window
+    {
+        Person _person = new Person()
+        {
+            Name = "Jamie",
+            Email = "Jamie@123.net",
+            Address = "Wall Street",
+            Number = 12345,
+        };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.DataContext = _person;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"{_person.Name}");
+        }
+    }
+
+    class Person
+    {
+        public Person()
+        {
+            
+        }
+
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public int? Number { get; set; }
+    }
+}
