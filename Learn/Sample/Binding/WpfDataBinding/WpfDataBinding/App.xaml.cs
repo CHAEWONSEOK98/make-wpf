a6b219724cab7e64bf06ef56c7692c25369f2383
CHAEWONSEOK98 <7dugod7@naver.com>
2024-08-10 14:50:04 +0900

    Sample - DataBinding

---
+++ Learn/Sample/Binding/WpfDataBinding/WpfDataBinding/App.xaml.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows;
+
+namespace WpfDataBinding
+{
+    public partial class App : Application
+    {
+    }
+}
---
+++ Learn/Sample/Binding/WpfDataBinding/WpfDataBinding/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+
+namespace WpfDataBinding
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class Student
+    {
+        public int Id { get; set; } = 1;
+        public string Name { get; set; } = "Chaegyul";
+        public List<Book> Books { get; set; }
+
+        public Student()
+        {
+            Books = new List<Book>()
+            {
+                new Book()
+                {
+                    Id=7, Title="Databinding In WPF"
+                },
+                new Book()
+                {
+                    Id=8, Title="DataContext In WPF"
+                }
+            };
+        }
+    }
+
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+}
