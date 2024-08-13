cbe64d77caa232c7817e93406e0f2dcc51eb052d
CHAEWONSEOK98 <7dugod7@naver.com>
2024-08-13 16:07:19 +0900

    XML File - Add Record To XML

--- C#/File/XMLDemoApp/XMLDemoApp/Program.cs
+++ C#/File/XMLDemoApp/XMLDemoApp/Program.cs
@@ -6,8 +6,28 @@ namespace XMLDemoApp
     {
         static void Main(string[] args)
         {
-            CreateXMLFile();
+            AddRecordToXML();
+            //CreateXMLFile();
             Console.ReadLine();
+        }
+
+        static void AddRecordToXML()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(@"C:\\Users\\7dugo\\Desktop\\make-wpf\\C#\\File\\XMLDemoApp\\XMLDemoApp\\movies.xml");
+            XmlNode root = doc.SelectSingleNode("MOVIES");
+            XmlElement movie = doc.CreateElement("MOVIE");
+            root.AppendChild(movie);
+
+            XmlAttribute id = doc.CreateAttribute("id");
+            id.Value = doc.SelectNodes("MOVIES/MOVIE").Count.ToString();
+            movie.Attributes.Append(id);
+
+            XmlElement title = doc.CreateElement("TITLE");
+            title.InnerText = "Lord Of The Rings";
+            movie.AppendChild(title);
+            doc.Save(@"C:\\Users\\7dugo\\Desktop\\make-wpf\\C#\\File\\XMLDemoApp\\XMLDemoApp\\movies.xml");
+            Console.WriteLine(doc.InnerXml);
         }
 
         static void CreateXMLFile()
@@ -15,7 +35,7 @@ namespace XMLDemoApp
             XmlDocument doc = new XmlDocument();
             XmlElement root = doc.CreateElement("MOVIES");
             doc.AppendChild(root);
-            doc.Save(@"C:\\Users\\7dugo\\Desktop\\make-wpf\\C#\\File\\XMLDemoApp\\XMLDemoApp\\movies.cs");
+            doc.Save(@"C:\\Users\\7dugo\\Desktop\\make-wpf\\C#\\File\\XMLDemoApp\\XMLDemoApp\\movies.xml");
             System.Console.WriteLine(doc.InnerXml);
         }
     }
