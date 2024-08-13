8090786797f07f71b4c48b8169917f15d4c9e699
CHAEWONSEOK98 <7dugod7@naver.com>
2024-08-14 00:32:07 +0900

    Text File - Create, Read, Update

---
+++ C#/File/TextFileDemo/TextFileDemo/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace TextFileDemo
+{
+    class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    //Write();
+        //    //Read();
+        //    Append();
+        //    Console.ReadLine();
+        //}
+
+        const string filename = "example.txt";
+
+        static void Write()
+        {
+            StreamWriter sw = new StreamWriter(filename);
+            sw.WriteLine("line 0");
+            string s = "123456789a";
+            sw.WriteLine(s);
+            sw.Close();
+        }
+
+        static void Append()
+        {
+            StreamWriter sw = new StreamWriter(filename, true);
+            for(int i = 0; i < 10; i++)
+            {
+                sw.Write(i);
+            }
+            sw.Close();
+            Read();
+        }
+
+        static void Read()
+        {
+            StreamReader sr = new StreamReader(filename);
+            //string s = sr.ReadLine();
+            //while (sr.ReadLine() is string s)
+            //{
+            //    Console.WriteLine(s);
+            //}
+            string s = sr.ReadToEnd();
+            Console.WriteLine(s);
+            sr.Close();
+        }
+    }
+}
---
+++ C#/File/TextFileDemo/TextFileDemo/Program2.cs
@@ -0,0 +1,54 @@
+﻿namespace TextFileDemo
+{
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            //Write();
+            //Append();
+            Read();
+            Console.ReadLine();
+        }
+
+        const string filename = "example2.txt";
+        static void Write()
+        {
+            //string str = "part-2";
+            //File.WriteAllText(filename, str);
+
+            string[] strArray = { "Creates the file", "writes the content", "Closes the file", "if exists then overwrites" };
+            File.WriteAllLines(filename, strArray);
+        }
+
+        static void Append()
+        {
+            //string content = "Opens the file, Writes to the file, Closes the file; " +
+            //    "If does not exist, it creates the file, then writes, then closes";
+            //File.AppendAllText(filename, content);
+
+            string[] contents = {"Opens the file,", "Writes to the file in lines,", "Closes the file;",
+            "If does not exist,", "it creates the file,", "then writes, then closes"};
+            File.AppendAllLines(filename, contents);
+        }
+
+        static void Read()
+        {
+            try
+            {
+                //string content = File.ReadAllText(filename);
+                //Console.WriteLine(content);
+
+                string[] lines = File.ReadAllLines(filename);
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    Console.WriteLine(i + " " + lines[i]);
+                }
+            }   
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                File.AppendAllText("errors.txt",DateTime.Now + " " + ex.Message);
+            }
+        }
+    }
+}
