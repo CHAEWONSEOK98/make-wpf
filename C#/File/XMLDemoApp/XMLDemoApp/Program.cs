7ffecc291c659e46d8840f0106a8b8375aadd0ba
CHAEWONSEOK98 <7dugod7@naver.com>
2024-08-13 14:07:31 +0900

    XML File - Create XML File

---
+++ C#/File/XMLDemoApp/XMLDemoApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Xml;
+
+namespace XMLDemoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateXMLFile();
+            Console.ReadLine();
+        }
+
+        static void CreateXMLFile()
+        {
+            XmlDocument doc = new XmlDocument();
+            XmlElement root = doc.CreateElement("MOVIES");
+            doc.AppendChild(root);
+            doc.Save(@"C:\\Users\\7dugo\\Desktop\\make-wpf\\C#\\File\\XMLDemoApp\\XMLDemoApp\\movies.cs");
+            System.Console.WriteLine(doc.InnerXml);
+        }
+    }
+}
