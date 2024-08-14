53a69af8262804c1b38e96897065b04ffcc27f37
CHAEWONSEOK98 <7dugod7@naver.com>
2024-08-14 18:33:22 +0900

    Template Sample - ControlTemplate StaticResource DynamicResource

---
+++ Learn/Sample/Template/WpfControlTemplate/WpfControlTemplate/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+
+namespace WpfControlTemplate
+{
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Learn/Sample/Template/WpfControlTemplate/WpfControlTemplate/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace WpfControlTemplate
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
