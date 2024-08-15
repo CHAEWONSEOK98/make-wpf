9982e0393499f6aea76111151705dc72a7f663a3
CHAEWONSEOK98 <7dugod7@naver.com>
2024-08-15 20:35:45 +0900

    Template Sample - DataTemplateSelector, ItemTemplateSelector

---
+++ Learn/Sample/Template/WpfDataTemplate/WpfDataTemplate/MyDataTemplateSelector.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfDataTemplate
+{
+    class MyDataTemplateSelector : DataTemplateSelector
+    {
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            FrameworkElement element = container as FrameworkElement;
+            if(element != null && item is Product p)
+            {
+                if(p.Dep == Dep.Dairy)
+                {
+                    return element.FindResource("MyProductDT2") as DataTemplate;
+                }
+                else if (p.Dep == Dep.Snack)
+                {
+                    return element.FindResource("MyProductDT3") as DataTemplate;
+                }
+                else if (p.Dep == Dep.Soda)
+                {
+                    return element.FindResource("MyProductDT4") as DataTemplate;
+                }
+            }
+            return element.FindResource("MyProductDTwithEnum") as DataTemplate;
+        }
+    }
+}
