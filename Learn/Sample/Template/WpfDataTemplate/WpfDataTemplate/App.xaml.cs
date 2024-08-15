20695e1086033d81ad9911ff82f707071d384c1e
CHAEWONSEOK98 <7dugod7@naver.com>
2024-08-15 16:51:16 +0900

    Template Sample - DataTemplate Basic

---
+++ Learn/Sample/Template/WpfDataTemplate/WpfDataTemplate/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+
+namespace WpfDataTemplate
+{
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Learn/Sample/Template/WpfDataTemplate/WpfDataTemplate/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Windows;
+
+namespace WpfDataTemplate
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class Products
+    {
+        public List<Product> MyProducts { get; set; } = GetProducts();
+        public static List<Product> GetProducts()
+        {
+            return new List<Product>()
+            {
+                new Product(){Id=1, ProductName="Milk", Dep=Dep.Dairy },
+                new Product(){Id=2, ProductName="Juice", Dep=Dep.Dairy },
+                new Product(){Id=3, ProductName="Twizzlers", Dep=Dep.Candy },
+                new Product(){Id=1, ProductName="Candy Bar", Dep=Dep.Candy },
+                new Product(){Id=2, ProductName="Chips", Dep=Dep.Snack },
+                new Product(){Id=3, ProductName="Gingerale", Dep=Dep.Soda },
+            };
+        }
+
+        public Product Product { get; set; } = new Product()
+        {
+            Id = 20,
+            ProductName = "Ice Cream"
+        };
+    }
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public Dep Dep { get; set; }
+    }
+
+    public enum Dep
+    {
+        Dairy,Snack,Candy,Soda
+    }
+}
