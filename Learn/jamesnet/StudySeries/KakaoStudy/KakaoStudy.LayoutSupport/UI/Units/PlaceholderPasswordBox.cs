6fcb25c5f1f005df9820246ab257c5aa6aeac219
CHAEWONSEOK98 <7dugod7@naver.com>
2024-09-04 11:44:37 +0900

    Kakao - Create PlaceholderPasswordBox and Design with OnApplyTemplate

---
+++ Learn/jamesnet/StudySeries/KakaoStudy/KakaoStudy.LayoutSupport/UI/Units/PlaceholderPasswordBox.cs
@@ -0,0 +1,52 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace KakaoStudy.LayoutSupport.UI.Units
+{
+    public class PlaceholderPasswordBox : TextBox
+    {
+        public static readonly DependencyProperty PlaceholderTextProperty =
+    DependencyProperty.Register(nameof(PlaceholderText), typeof(string), typeof(PlaceholderPasswordBox), new PropertyMetadata(""));
+
+        public string PlaceholderText
+        {
+            get { return (string)GetValue(PlaceholderTextProperty); }
+            set { SetValue(PlaceholderTextProperty, value); }
+        }
+
+        static PlaceholderPasswordBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(PlaceholderPasswordBox), new FrameworkPropertyMetadata(typeof(PlaceholderPasswordBox)));
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            if (GetTemplateChild("PART_PasswordBox") is PasswordBox pwd)
+            {
+                pwd.PasswordChanged += Pwd_PasswordChanged;
+            }
+        }
+
+        private void Pwd_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (sender is PasswordBox pwd)
+            {
+                SetValue(TextProperty, pwd.Password);
+            }
+        }
+
+        private void CustomPasswordBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (e.OriginalSource == this)
+            {
+                PasswordBox passwordBox = GetTemplateChild("PART_PasswordBox") as PasswordBox;
+                if (passwordBox != null)
+                {
+                    passwordBox.Focus();
+                }
+            }
+        }
+    }
+}
