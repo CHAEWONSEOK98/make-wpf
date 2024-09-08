336c933fec30b1536679646a0521622fc43d9ade
CHAEWONSEOK98 <7dugod7@naver.com>
2024-09-09 01:40:05 +0900

    WpfExplorer: init

---
+++ Learn/jamesnet/StudySeries/WpfExplorer/WpfExplorer.Forms/UI/Views/ExplorerWinodw.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfExplorer.Forms.UI.Views
+{
+    public class ExplorerWinodw : Window
+    {
+        static ExplorerWinodw()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ExplorerWinodw), new FrameworkPropertyMetadata(typeof(ExplorerWinodw)));
+        }
+    }
+}
---
+++ Learn/jamesnet/StudySeries/WpfExplorer/WpfExplorer.Support/UI/Units/DarkWindow.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfExplorer.Support.UI.Units
+{
+    public class DarkWindow : Window
+    {
+        static DarkWindow()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(DarkWindow), new FrameworkPropertyMetadata(typeof(DarkWindow)));
+        }
+
+        public DarkWindow()
+        {
+            WindowStyle = WindowStyle.None;
+            AllowsTransparency = true;
+        }
+    }
+}
