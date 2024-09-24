7346a1c583693e505e6ad39bbdd1cd4794ef8db1
CHAEWONSEOK98 <7dugod7@naver.com>
2024-09-25 08:32:20 +0900

    tutorial #1 Switching Views

---
+++ Learn/SingletonSean/WPF_NAVIGATION/Tutorial_1_SwitchingViews/NavigationMVVM/ViewModels/HomeViewModel.cs
@@ -0,0 +1,18 @@
+﻿using NavigationMVVM.Commands;
+using NavigationMVVM.Stores;
+using System.Windows.Input;
+
+namespace NavigationMVVM.ViewModels
+{
+    public class HomeViewModel : ViewModelBase
+    {
+        public string WelcomeMessage => "Welcome to my application.";
+
+        public ICommand NavigateAccountCommand { get; }
+
+        public HomeViewModel(NavigationStore navigationStore)
+        {
+            NavigateAccountCommand = new NavigateCommand<AccountViewModel>(navigationStore, () => new AccountViewModel(navigationStore));
+        }
+    }
+}
