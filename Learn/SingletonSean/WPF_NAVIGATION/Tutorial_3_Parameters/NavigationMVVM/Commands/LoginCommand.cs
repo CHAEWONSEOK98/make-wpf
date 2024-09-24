3e96ff678bc6db2de713b97565b9703dc762065d
CHAEWONSEOK98 <7dugod7@naver.com>
2024-09-25 08:35:30 +0900

    tutorial #3 Parameters

---
+++ Learn/SingletonSean/WPF_NAVIGATION/Tutorial_3_Parameters/NavigationMVVM/Commands/LoginCommand.cs
@@ -0,0 +1,36 @@
+﻿using NavigationMVVM.Models;
+using NavigationMVVM.Services;
+using NavigationMVVM.Stores;
+using NavigationMVVM.ViewModels;
+using System.Windows;
+using System.Windows.Navigation;
+
+namespace NavigationMVVM.Commands
+{
+    public class LoginCommand : CommandBase
+    {
+        private readonly LoginViewModel _viewModel;
+        private readonly AccountStore _accountStore;
+        private readonly NavigationService<AccountViewModel> _navigationService;
+
+        public LoginCommand(LoginViewModel viewModel, AccountStore accountStore, NavigationService<AccountViewModel> navigationService)
+        {
+            _viewModel = viewModel;
+            _accountStore = accountStore;
+            _navigationService = navigationService;
+        }
+
+        public override void Execute(object? parameter)
+        {
+            Account account = new Account()
+            {
+                Email = $"{_viewModel.Username}@test.com",
+                Username = _viewModel.Username
+            };
+
+            _accountStore.CurrentAccount = account;
+
+            _navigationService.Navigate();
+        }
+    }
+}
---
+++ Learn/SingletonSean/WPF_NAVIGATION/Tutorial_3_Parameters/NavigationMVVM/ViewModels/AccountViewModel.cs
@@ -0,0 +1,26 @@
+﻿using NavigationMVVM.Commands;
+using NavigationMVVM.Models;
+using NavigationMVVM.Services;
+using NavigationMVVM.Stores;
+using System.Windows.Input;
+
+namespace NavigationMVVM.ViewModels
+{
+    public class AccountViewModel : ViewModelBase
+    {
+        private readonly AccountStore _accountStore;
+
+        public string Username => _accountStore.CurrentAccount?.Username;
+
+        public string Email => _accountStore.CurrentAccount?.Email;
+
+        public ICommand NavigateHomeCommand { get; }
+
+        public AccountViewModel(AccountStore accountStore, NavigationStore navigationStore)
+        {
+            _accountStore = accountStore;
+
+            NavigateHomeCommand = new NavigateCommand<HomeViewModel>(new NavigationService<HomeViewModel>(navigationStore, () => new HomeViewModel(accountStore, navigationStore)));
+        }
+    }
+}
