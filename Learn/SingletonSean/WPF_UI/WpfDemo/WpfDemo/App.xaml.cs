f527e3b2b1e18e14a9e40d4a9a5f78445a8ae49b
CHAEWONSEOK98 <7dugod7@naver.com>
2024-09-27 13:57:56 +0900

    Highlight Text Block

---
+++ Learn/SingletonSean/WPF_UI/WpfDemo/WpfDemo/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using WpfDemo.Windows;
+
+namespace WpfDemo
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            MainWindow = new HighlightTextBlockDemoWindow();
+            MainWindow.Show();
+
+            base.OnStartup(e);
+        }
+    }
+}
---
+++ Learn/SingletonSean/WPF_UI/WpfDemo/WpfDemo/Windows/HighlightTextBlockDemoWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace WpfDemo.Windows
+{
+    public partial class HighlightTextBlockDemoWindow : Window
+    {
+        public HighlightTextBlockDemoWindow()
+        {
+            InitializeComponent();
+
+            lvItems.ItemsSource = new ObservableCollection<string>()
+            {
+                "Hello World",
+                "Name is",
+                "Chaegyul",
+                "and my cat",
+                "king cat",
+            };
+        }
+    }
+}
