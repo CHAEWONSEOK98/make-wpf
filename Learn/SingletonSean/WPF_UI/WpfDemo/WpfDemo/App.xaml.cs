45d7080fd7fc765427805d33a0f7efa7ae6a9ee2
CHAEWONSEOK98 <7dugod7@naver.com>
2024-10-01 22:51:07 +0900

    modal

--- Learn/SingletonSean/WPF_UI/WpfDemo/WpfDemo/App.xaml.cs
+++ Learn/SingletonSean/WPF_UI/WpfDemo/WpfDemo/App.xaml.cs
@@ -7,7 +7,7 @@ namespace WpfDemo
     {
         protected override void OnStartup(StartupEventArgs e)
         {
-            MainWindow = new DropdownMenuDemoWindow();
+            MainWindow = new ModalDemoWindow();
             MainWindow.Show();
 
             base.OnStartup(e);
