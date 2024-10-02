63533b6bdd4e3d6889e72ee31a2946b6095e8232
CHAEWONSEOK98 <7dugod7@naver.com>
2024-10-02 22:49:49 +0900

    Dependency Property Metadata

--- Learn/SingletonSean/WPF_Custom_Controls/AnalogClock/AnalogClock/CustomControls/Clock.cs
+++ Learn/SingletonSean/WPF_Custom_Controls/AnalogClock/AnalogClock/CustomControls/Clock.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
 
@@ -10,7 +11,47 @@ namespace AnalogClock.CustomControls
     [TemplateVisualState(Name = "Christmas", GroupName = "TimeStates")]
     public class Clock : Control
     {
-        public static readonly DependencyProperty TimeProperty = DependencyProperty.Register("Time", typeof(DateTime), typeof(Clock), new PropertyMetadata(DateTime.Now));
+        public static readonly DependencyProperty TimeProperty = DependencyProperty.Register("Time", typeof(DateTime), typeof(Clock), new PropertyMetadata(DateTime.Now, TimePropertyChanged, TimeCoerceValue));
+
+        private static object TimeCoerceValue(DependencyObject d, object baseValue)
+        {
+            if(baseValue is DateTime)
+            {
+                DateTime time = (DateTime)baseValue;
+
+                if(time.Second % 2 == 1)
+                {
+                    baseValue = time.AddSeconds(1);
+                }
+            }
+
+            return baseValue;
+        }
+
+        private static bool TimeValidateValue(object value)
+        {
+            if(value is DateTime)
+            {
+                DateTime time = (DateTime)value;
+
+                if(time.Second % 2 == 1)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static void TimePropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if(d is Clock)
+            {
+                Clock clock = d as Clock;
+                clock.RaiseEvent(new RoutedPropertyChangedEventArgs<DateTime>((DateTime)e.OldValue, (DateTime)e.NewValue, TimeChangedEvent));
+            }
+        }
+
         public static DependencyProperty ShowSecondsProperty = DependencyProperty.Register("ShowSeconds", typeof(bool), typeof(Clock), new PropertyMetadata(true));
         public static RoutedEvent TimeChangedEvent = EventManager.RegisterRoutedEvent("TimeChanged", RoutingStrategy.Bubble, typeof(RoutedPropertyChangedEventHandler<DateTime>), typeof(Clock));
 
@@ -53,9 +94,6 @@ namespace AnalogClock.CustomControls
         protected virtual void OnTimeChanged(DateTime newTime)
         {
             UpdateTimeState(newTime);
-
-            // 이벤트 발생
-            RaiseEvent(new RoutedPropertyChangedEventArgs<DateTime>(Time, newTime, TimeChangedEvent));
             Time = newTime;
         }
 
