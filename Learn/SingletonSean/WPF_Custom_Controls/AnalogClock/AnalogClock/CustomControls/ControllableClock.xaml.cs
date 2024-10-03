89c9f69042a9f9214ab8b504c777727962a68d53
CHAEWONSEOK98 <7dugod7@naver.com>
2024-10-03 23:04:27 +0900

    Composition Over Inheritance

---
+++ Learn/SingletonSean/WPF_Custom_Controls/AnalogClock/AnalogClock/CustomControls/ControllableClock.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace AnalogClock.CustomControls
+{
+    public partial class ControllableClock : UserControl
+    {
+        public Clock Clock
+        {
+            get { return (Clock)GetValue(ClockProperty); }
+            set { SetValue(ClockProperty, value); }
+        }
+
+        public static readonly DependencyProperty ClockProperty =
+            DependencyProperty.Register("Clock", typeof(Clock), typeof(ControllableClock), new PropertyMetadata(null));
+
+        public ControllableClock()
+        {
+            InitializeComponent();
+        }
+    }
+}
