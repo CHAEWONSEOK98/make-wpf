4e5595004575ace520f783da6b667c7ad058483b
CHAEWONSEOK98 <7dugod7@naver.com>
2024-10-07 23:22:18 +0900

    Views

---
+++ Learn/SingletonSean/WPF_Application/YouTubeViewers/YouTubeViewers/Components/YouTubeViewersDetails.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace YouTubeViewers.Components
+{
+    public partial class YouTubeViewersDetails : UserControl
+    {
+        public YouTubeViewersDetails()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Learn/SingletonSean/WPF_Application/YouTubeViewers/YouTubeViewers/Views/YouTubeViewersView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace YouTubeViewers.Views
+{
+    public partial class YouTubeViewersView : UserControl
+    {
+        public YouTubeViewersView()
+        {
+            InitializeComponent();
+        }
+    }
+}
