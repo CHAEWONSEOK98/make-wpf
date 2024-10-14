e667cc9ec464777afb80be22d621e09fd8f137e1
CHAEWONSEOK98 <7dugod7@naver.com>
2024-10-14 18:07:09 +0900

    Entity Framework Setup

---
+++ Learn/SingletonSean/WPF_Application/YouTubeViewers/YouTubeViewers.EntityFramework/DTOs/YouTubeViewerDto.cs
@@ -0,0 +1,13 @@
+﻿namespace YouTubeViewers.EntityFramework.DTOs
+{
+    public class YouTubeViewerDto
+    {
+        public Guid Id { get; set; }
+
+        public string Username { get; set; }
+
+        public bool IsSubscribed { get; set; }
+
+        public bool IsMember { get; set; }
+    }
+}
