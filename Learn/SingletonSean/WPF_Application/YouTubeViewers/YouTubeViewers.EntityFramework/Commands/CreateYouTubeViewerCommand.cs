a515ef8a0b7fb506fab1e363521ff709e732b11d
CHAEWONSEOK98 <7dugod7@naver.com>
2024-10-15 14:02:33 +0900

    Services

---
+++ Learn/SingletonSean/WPF_Application/YouTubeViewers/YouTubeViewers.EntityFramework/Commands/CreateYouTubeViewerCommand.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using YouTubeViewers.Domain.Commands;
+using YouTubeViewers.Domain.Models;
+using YouTubeViewers.EntityFramework.DTOs;
+
+namespace YouTubeViewers.EntityFramework.Commands
+{
+    public class CreateYouTubeViewerCommand : ICreateYouTubeViewerCommand
+    {
+        private readonly YouTubeViewersDbContextFactory _contextFactory;
+
+        public CreateYouTubeViewerCommand(YouTubeViewersDbContextFactory contextFactory)
+        {
+            _contextFactory = contextFactory;
+        }
+
+        public async Task Execute(YouTubeViewer youTubeViewer)
+        {
+            using (YouTubeViewersDbContext context = _contextFactory.Create())
+            {
+                YouTubeViewerDto youTubeViewerDto = new YouTubeViewerDto()
+                {
+                    Id = youTubeViewer.Id,
+                    Username = youTubeViewer.Username,
+                    IsSubscribed = youTubeViewer.IsSubscribed,
+                    IsMember = youTubeViewer.IsMember,
+                };
+
+                context.YouTubeViewers.Add(youTubeViewerDto);
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
--- Learn/SingletonSean/WPF_Application/YouTubeViewers/YouTubeViewers/App.xaml.cs
+++ Learn/SingletonSean/WPF_Application/YouTubeViewers/YouTubeViewers/App.xaml.cs
@@ -1,4 +1,10 @@
-﻿using System.Windows;
+﻿using Microsoft.EntityFrameworkCore;
+using System.Windows;
+using YouTubeViewers.Domain.Commands;
+using YouTubeViewers.Domain.Queries;
+using YouTubeViewers.EntityFramework;
+using YouTubeViewers.EntityFramework.Commands;
+using YouTubeViewers.EntityFramework.Queries;
 using YouTubeViewers.Stores;
 using YouTubeViewers.ViewModels;
 
@@ -7,13 +13,37 @@ namespace YouTubeViewers
     public partial class App : Application
     {
         private readonly ModalNavigationStore _modalNavigationStore;
+
+        private readonly YouTubeViewersDbContextFactory _youTubeViewersDbContextFactory;
+        private readonly IGetAllYouTubeViewersQuery _getAllYouTubeViewersQuery;
+        private readonly ICreateYouTubeViewerCommand _createYouTubeViewerCommand;
+        private readonly IUpdateYouTubeViewerCommand _updateYouTubeViewerCommand;
+        private readonly IDeleteYouTubeViewerCommand _deleteYouTubeViewerCommand;
+
         private readonly YouTubeViewersStore _youTubeViewersStore;
         private readonly SelectedYouTubeViewerStore _selectedYouTubeViewerStore;
 
         public App()
         {
+            string connectionString = "Data Source=YouTubeViewers.db";
+
             _modalNavigationStore = new ModalNavigationStore();
-            _youTubeViewersStore = new YouTubeViewersStore();
+
+            _youTubeViewersDbContextFactory = new YouTubeViewersDbContextFactory
+                (
+                new DbContextOptionsBuilder().UseSqlite(connectionString).Options
+                );
+            _getAllYouTubeViewersQuery = new GetAllYouTubeViewersQuery(_youTubeViewersDbContextFactory);
+            _createYouTubeViewerCommand = new CreateYouTubeViewerCommand(_youTubeViewersDbContextFactory);
+            _updateYouTubeViewerCommand = new UpdateYouTubeViewerCommand(_youTubeViewersDbContextFactory);
+            _deleteYouTubeViewerCommand = new DeleteYouTubeViewerCommand(_youTubeViewersDbContextFactory);
+
+            _youTubeViewersStore = new YouTubeViewersStore(
+                _getAllYouTubeViewersQuery,
+                _createYouTubeViewerCommand,
+                _updateYouTubeViewerCommand,
+                _deleteYouTubeViewerCommand);
+
             _selectedYouTubeViewerStore = new SelectedYouTubeViewerStore(_youTubeViewersStore);
         }
 
